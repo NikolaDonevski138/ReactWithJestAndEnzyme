84817964de684766754c05bceb3530a38cda1ec1
unknown <n.donevski@yahoo.com>
2020-08-05 12:50:40 +0200

    Initial Commit of My components without tests

---
+++ src/components/header/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles.scss'
+
+const Header = () => {
+    return (
+        <header>
+            <div className="wrap">
+                <div className="logo">
+                    <p className="colorOfLogo">Logo</p>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
